57db118ceaa5a4a06080dc41fd98f8ae6210b61e
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-11 11:29:46 -0600

    recreating bean inheritance

--- src/main/java/org/snva/helloworld/beans/Role.java
+++ src/main/java/org/snva/helloworld/beans/Role.java
@@ -1,87 +1,56 @@
 package org.snva.helloworld.beans;
 
 public class Role {
-//    private long roleId;
-//
-//
-//    public long getRoleId() {
-//        return roleId;
-//    }
-//
-//    public void setRoleId(long roleId) {
-//        this.roleId = roleId;
-//    }
-//
-//    public String getRoleName() {
-//        return roleName;
-//    }
-//
-//    public void setRoleName(String roleName) {
-//        this.roleName = roleName;
-//    }
-//
-//    public String getRoleDescription() {
-//        return roleDescription;
-//    }
-//
-//    public void setRoleDescription(String roleDescription) {
-//        this.roleDescription = roleDescription;
-//    }
-//
-//    private String roleName;
-//    private String roleDescription;
-//
-//    @Override
-//    public String toString() {
-//        return "Role{" +
-//                "roleId=" + roleId +
-//                ", roleName='" + roleName + '\'' +
-//                ", roleDescription='" + roleDescription + '\'' +
-//                '}';
-//    }
-//
-//    public void init(){
-//        System.out.println("Role has been created");
-//    }
-//
-//    public void destroy(){
-//        System.out.println(this.roleName + "with has been destroyed" );
-//    }
+    private long roleId;
 
-    private String learner;
-    private String instructor;
-    private String reason;
 
-    public String getLearner() {
-        return learner;
+    public long getRoleId() {
+        return roleId;
     }
 
-    public void setLearner(String learner) {
-        this.learner = learner;
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
     }
 
-    public String getInstructor() {
-        return instructor;
+    public String getRoleName() {
+        return roleName;
     }
 
-    public void setInstructor(String instructor) {
-        this.instructor = instructor;
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
     }
 
-    public String getReason() {
-        return reason;
+    public String getRoleDescription() {
+        return roleDescription;
     }
 
-    public void setReason(String reason) {
-        this.reason = reason;
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription;
     }
+
+    private String roleName;
+    private String roleDescription;
 
     @Override
     public String toString() {
         return "Role{" +
-                "learner='" + learner + '\'' +
-                ", instructor='" + instructor + '\'' +
-                ", reason='" + reason + '\'' +
+                "roleId=" + roleId +
+                ", roleName='" + roleName + '\'' +
+                ", roleDescription='" + roleDescription + '\'' +
                 '}';
     }
+
+    public void init(){
+        System.out.println("Role has been created");
+    }
+
+    public void destroy(){
+        System.out.println(this.roleName + "with has been destroyed" );
+    }
+
+
+
+
+
+
 }
--- src/main/java/org/snva/helloworld/beans/User.java
+++ src/main/java/org/snva/helloworld/beans/User.java
@@ -3,129 +3,94 @@ package org.snva.helloworld.beans;
 import java.util.List;
 
 public class User {
-//     private String userId;
-//     private String userName;
-//     private String firstName;
-//     private String lastName;
-//
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "userId='" + userId + '\'' +
-//                ", userName='" + userName + '\'' +
-//                ", firstName='" + firstName + '\'' +
-//                ", lastName='" + lastName + '\'' +
-//                ", contact='" + contact + '\'' +
-//                ", email='" + email + '\'' +
-//                ", password='" + password + '\'' +
-//                ", role=" + role +
-//                '}';
-//    }
-//
-//    private String contact;
-//     private String email;
-//
-//    public String getUserId() {
-//        return userId;
-//    }
-//
-//    public void setUserId(String userId) {
-//        this.userId = userId;
-//    }
-//
-//    public String getUserName() {
-//        return userName;
-//    }
-//
-//    public void setUserName(String userName) {
-//        this.userName = userName;
-//    }
-//
-//    public String getFirstName() {
-//        return firstName;
-//    }
-//
-//    public void setFirstName(String firstName) {
-//        this.firstName = firstName;
-//    }
-//
-//    public String getLastName() {
-//        return lastName;
-//    }
-//
-//    public void setLastName(String lastName) {
-//        this.lastName = lastName;
-//    }
-//
-//    public String getContact() {
-//        return contact;
-//    }
-//
-//    public void setContact(String contact) {
-//        this.contact = contact;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public List<Role> getRole() {
-//        return role;
-//    }
-//
-//    public void setRole(List<Role> role) {
-//        this.role = role;
-//    }
-//
-//    private String password;
-//     private List <Role>role;
-
-    private String learner;
-    private String instructor;
-    private String message;
-
-    public String getLearner() {
-        return learner;
-    }
-
-    public void setLearner(String learner) {
-        this.learner = learner;
-    }
-
-    public String getInstructor() {
-        return instructor;
-    }
-
-    public void setInstructor(String instructor) {
-        this.instructor = instructor;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+     private String userId;
+     private String userName;
+     private String firstName;
+     private String lastName;
 
     @Override
     public String toString() {
         return "User{" +
-                "learner='" + learner + '\'' +
-                ", instructor='" + instructor + '\'' +
-                ", message='" + message + '\'' +
+                "userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", contact='" + contact + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
                 '}';
     }
+
+    private String contact;
+     private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(List<Role> role) {
+        this.role = role;
+    }
+
+    private String password;
+     private List <Role>role;
+
+
 }
