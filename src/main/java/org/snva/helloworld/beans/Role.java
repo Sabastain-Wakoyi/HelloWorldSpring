003833ca3769f455b027064dd056f00cd61ccd48
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-11 11:05:15 -0600

    bean inheritance

--- src/main/java/org/snva/helloworld/beans/Role.java
+++ src/main/java/org/snva/helloworld/beans/Role.java
@@ -1,50 +1,87 @@
 package org.snva.helloworld.beans;
 
 public class Role {
-    private long roleId;
+//    private long roleId;
+//
+//
+//    public long getRoleId() {
+//        return roleId;
+//    }
+//
+//    public void setRoleId(long roleId) {
+//        this.roleId = roleId;
+//    }
+//
+//    public String getRoleName() {
+//        return roleName;
+//    }
+//
+//    public void setRoleName(String roleName) {
+//        this.roleName = roleName;
+//    }
+//
+//    public String getRoleDescription() {
+//        return roleDescription;
+//    }
+//
+//    public void setRoleDescription(String roleDescription) {
+//        this.roleDescription = roleDescription;
+//    }
+//
+//    private String roleName;
+//    private String roleDescription;
+//
+//    @Override
+//    public String toString() {
+//        return "Role{" +
+//                "roleId=" + roleId +
+//                ", roleName='" + roleName + '\'' +
+//                ", roleDescription='" + roleDescription + '\'' +
+//                '}';
+//    }
+//
+//    public void init(){
+//        System.out.println("Role has been created");
+//    }
+//
+//    public void destroy(){
+//        System.out.println(this.roleName + "with has been destroyed" );
+//    }
 
+    private String learner;
+    private String instructor;
+    private String reason;
 
-    public long getRoleId() {
-        return roleId;
+    public String getLearner() {
+        return learner;
     }
 
-    public void setRoleId(long roleId) {
-        this.roleId = roleId;
+    public void setLearner(String learner) {
+        this.learner = learner;
     }
 
-    public String getRoleName() {
-        return roleName;
+    public String getInstructor() {
+        return instructor;
     }
 
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
     }
 
-    public String getRoleDescription() {
-        return roleDescription;
+    public String getReason() {
+        return reason;
     }
 
-    public void setRoleDescription(String roleDescription) {
-        this.roleDescription = roleDescription;
+    public void setReason(String reason) {
+        this.reason = reason;
     }
-
-    private String roleName;
-    private String roleDescription;
 
     @Override
     public String toString() {
         return "Role{" +
-                "roleId=" + roleId +
-                ", roleName='" + roleName + '\'' +
-                ", roleDescription='" + roleDescription + '\'' +
+                "learner='" + learner + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", reason='" + reason + '\'' +
                 '}';
-    }
-
-    public void init(){
-        System.out.println("Role has been created");
-    }
-
-    public void destroy(){
-        System.out.println(this.roleName + "with has been destroyed" );
     }
 }
